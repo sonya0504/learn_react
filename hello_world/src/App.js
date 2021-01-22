174dfd3bb58ced7304c687dca887620276cf601c
Sonia <sonia.jarczak@deviniti.com>
2021-01-22 23:50:46 +0100

    Functional Components

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+// for named export
+// import { Greet } from './components/Greet';
+// for default export:
+import Greet from './components/Greet'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to Docs
-        </a>
-      </header>
+      <Greet />
     </div>
   );
 }
---
+++ hello_world/src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Greet () {
+    return <h1>Hello Default Export!</h1>
+};
+
+// arrow function and named export:
+
+// export const Greet = () => <h1>Hello Named Export</h1>;
+
+export default Greet;
