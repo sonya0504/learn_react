e93c418089684ffbfda7644c9e41d59aa343a7cf
Sonia <sonia.jarczak@deviniti.com>
2021-01-23 13:50:35 +0100

    State

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,29 +1,11 @@
 import './App.css';
+import Message from './components/Message';
 
-// for named export
-// import { Greet } from './components/Greet';
-
-// for default export:
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
-      <Greet >
-        <p>This is children props</p>
-      </Greet>
-      <Greet name='Clark' heroName='Superman'>
-          <button>Action!</button>
-      </Greet>
-      <Greet name='Diana' heroName='Wonder Woman' />
-      <Welcome name='Bruce' heroName='Batman'>
-        <p>This is children props</p>
-      </Welcome>
-      <Welcome name='Clark' heroName='Superman' />
-      <Welcome name='Diana' heroName='Wonder Woman' />
-      <Hello />
+     <Message />
     </div>
   );
 }
