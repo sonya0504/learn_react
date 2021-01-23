ca61ba9c2a7cebfdcb1fedd394bf20836461d119
Sonia <sonia.jarczak@deviniti.com>
2021-01-23 23:09:53 +0100

    Methods as props

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <EventBind />
+      <ParentComponent />
     </div>
   );
 }
