53567349088a9168e5c7e2005c920f6bc64fbcc5
Sonia <sonia.jarczak@deviniti.com>
2021-01-24 18:06:44 +0100

    Basics od Form Handling

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './appStyle.css';
-import styles from './appStyle.module.css';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <Stylesheet primary={true} />
-      <Inline />
-      <h2 className='error'>Error</h2>
-      <h2 className={styles.success}>Success</h2>
+      <Form />
     </div>
   );
 }
