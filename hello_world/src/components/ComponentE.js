845fd450a70a47546465db0d10b7fb8dfd4c1744
Sonia <sonia.jarczak@deviniti.com>
2021-02-02 14:29:01 +0100

    React Contextpart 2

--- hello_world/src/components/ComponentE.js
+++ hello_world/src/components/ComponentE.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import UserContext from './userContext';
 import ComponentF from './ComponentF';
 
  class ComponentE extends Component {
+
+    static contextType  = UserContext;
+
     render() {
         return (
+            <>
+            <h2>Component E context {this.context} </h2>
             <ComponentF />
+            </>
         )
     }
 }
+
+// ComponentE.contextType = UserContext;
 
 export default ComponentE
--- hello_world/src/components/userContext.js
+++ hello_world/src/components/userContext.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const UserContext = React.createContext();
+const UserContext = React.createContext('default value');
 const UserProvider = UserContext.Provider;
 const UserConsumer = UserContext.Consumer;
 
 export { UserProvider, UserConsumer };
+export default UserContext;
