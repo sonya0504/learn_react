f84f88cca68458aee6bf602ff9c0bee32fbf27c3
Sonia <sonia.jarczak@deviniti.com>
2021-02-09 09:50:42 +0100

    useState with array

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterThree />
+      <HookCounterFour /> 
     </div>
   );
 }
