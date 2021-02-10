724d93dbdeb864f2049fca81342ae606c3eb066e
Sonia <sonia.jarczak@deviniti.com>
2021-02-10 17:48:15 +0100

    Fetch date with useEffect part 3

--- hooks/src/components/DataFetching.js
+++ hooks/src/components/DataFetching.js
@@ -5,6 +5,11 @@ function DataFetching() {
 
     const [post, setPost] = useState({});
     const [id, setId] = useState(1);
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+
+    const handleClick = () => {
+        setIdFromButtonClick(id);
+    }
 
     useEffect(() => {
         axios
@@ -15,11 +20,12 @@ function DataFetching() {
              }
             )
             .catch(err => console.log(err))   
-    }, [id]);
+    }, [idFromButtonClick]);
 
     return (
         <div>
             <input type="text" value={id} onChange={e => setId(e.target.value)} />
+            <button type="button" onClick={handleClick}>Fetch post</button>
             <p>{post.title}</p>
         </div>
     )
